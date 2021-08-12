b902a381e71e16734bd3fe73d4254d9805a12f92
Artur Rejment <arturrejment1@gmail.com>
2021-08-13 00:45:35 +0200

    Changed some Product rows as not required

--- product/views.py
+++ product/views.py
@@ -1,9 +1,9 @@
 from rest_framework import generics, mixins
 from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 
-from .serializers import ProductSerializer
+from .serializers import ProductListSerializer, ProductDetailSerializer
 from .models import Product
 
 class ProductListView(generics.ListAPIView):
-	serializer_class = ProductSerializer
+	serializer_class = ProductListSerializer
 	queryset = Product.objects.prefetch_related('categories')
