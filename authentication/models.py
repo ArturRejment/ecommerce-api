a964eb37d8aa8ff3e87f4dcbf2cddde9291d00af
Artur Rejment <arturrejment1@gmail.com>
2021-08-16 21:50:41 +0200

    User model upgraded

--- authentication/models.py
+++ authentication/models.py
@@ -2,10 +2,16 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
+	first_name = models.CharField(max_length=255)
+	last_name = models.CharField(max_length=255)
 	name = models.CharField(max_length=255)
 	email = models.CharField(max_length=255, unique=True)
+	phone = models.CharField(max_length=255, unique=True)
 
-	required_fields = []
+	required_fields = ['first_name', 'last_name', 'email', 'phone']
+
+	def __str__(self):
+		return f'{self.first_name} {self.last_name}'
 
 
 
--- authentication/urls.py
+++ authentication/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path, include
-from .views import RegisterView
+
 
 urlpatterns = [
-	path('register', RegisterView.as_view(), name='register'),
 	path('', include('djoser.urls')),
 	path('', include('djoser.urls.authtoken')),
 ]
