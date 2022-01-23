f46c77314b2bf8753300118073cb0615efb108fd
Artur Rejment <arturrejment1@gmail.com>
2022-01-23 15:59:20 +0100

    feat: add uuid for order, display status

--- order/serializers.py
+++ order/serializers.py
@@ -8,14 +8,18 @@ from product.serializers import DiscountSerializer
 class OrderSerializer(serializers.ModelSerializer):
     cart = CartSerializer()
     discount_code = DiscountSerializer()
-    status = serializers.ChoiceField(choices=Order.STATUS_CHOICE)
+    status = serializers.SerializerMethodField()
 
     class Meta:
         model = Order
         fields = (
+            'order_number',
             'total_cost_net',
             'discount_code',
             'status',
             'cart',
             'user',
         )
+
+    def get_status(self, obj):
+        return obj.get_status_display()
